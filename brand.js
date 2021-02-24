5617a82fde1bab8e39954b2abd18efd1cd4f96d1
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-23 19:05:19 -0500

    Delete brand.js

--- brand.js
+++
@@ -1,7 +0,0 @@
-function hoverBanner() {
-  document.body.style.backgroundImage = "radial-gradient(#ADD8E6, #FFFFFF)";
-}
-
-function hoverMain() {
-  document.body.style.backgroundImage = "radial-gradient(#FFFFFF, #ADD8E6)";
-}
