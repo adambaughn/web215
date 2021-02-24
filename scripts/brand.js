5f77bb223a07da00715197a9849c01b8c5ebdc2f
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-23 19:26:32 -0500

    Add files via upload

---
+++ scripts/brand.js
@@ -0,0 +1,7 @@
+function hoverBanner() {
+  document.body.style.backgroundImage = "radial-gradient(#ADD8E6, #FFFFFF)";
+}
+
+function hoverMain() {
+  document.body.style.backgroundImage = "radial-gradient(#FFFFFF, #ADD8E6)";
+}
