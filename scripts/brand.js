86895b677772a1bc115f4e9e8c5a8e78edc77be4
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-23 22:37:06 -0500

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -5,3 +5,36 @@ function hoverBanner() {
 function hoverMain() {
   document.body.style.backgroundImage = "radial-gradient(#FFFFFF, #ADD8E6)";
 }
+
+ $(document).ready(function() {
+   $('#dynamiccontent').load('/about.html');
+
+   $('#dynamiclinks a').click(function(e) {
+     e.preventDefault();
+     $("#dynamiccontent").load(e.target.href);
+   })
+ });
+
+
+
+
+
+
+// function changeContent () {
+//  if ($("a").attr("id") === "contract") {
+    
+//  }
+//  else if ($("a").attr("id") === "introduction") {
+    
+// }
+//  else if ($("a").attr("id") === "codeplay") {
+    
+//  }
+//  else if ($("a").attr("id") === "asmarterwaytolearn") {
+    
+//  }
+//  else {
+    
+//  }
+//}
+  
