024b89cb0f900e3e874daa1146aeb9b970a282e6
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-24 13:43:58 -0500

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -6,35 +6,34 @@ function hoverMain() {
   document.body.style.backgroundImage = "radial-gradient(#FFFFFF, #ADD8E6)";
 }
 
- $(document).ready(function() {
-   $('#dynamiccontent').load('/about.html');
+function loadXMLDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("dynamiccontent").innerHTML =
+      this.responseText;
+    }
+  };
+  if ($("a").attr("id") === "contract") {
+    xhttp.open("GET", "xmlhttp_info.txt", true);
+    xhttp.send();
+  } 
+    else if ($("a").attr("id") === "introduction") {
+    xhttp.open("GET", "introduction", true);
+    xhttp.send();
+  } 
+    else if ($("a").attr("id") === "codeplay") {
+    xhttp.open("GET", "codeplay.txt", true);
+    xhttp.send();
+  } 
+    else if ($("a").attr("id") === "asmarterwaytolearn") {
+    xhttp.open("GET", "asmarterwaytolearn.txt", true);
+    xhttp.send();
+  } 
+    else {
+    xhttp.open("GET", "placeholder.txt.txt", true);
+    xhttp.send();
+}
 
-   $('#dynamiclinks a').click(function(e) {
-     e.preventDefault();
-     $("#dynamiccontent").load(e.target.href);
-   })
- });
 
-
-
-
-
-
-// function changeContent () {
-//  if ($("a").attr("id") === "contract") {
-    
-//  }
-//  else if ($("a").attr("id") === "introduction") {
-    
-// }
-//  else if ($("a").attr("id") === "codeplay") {
-    
-//  }
-//  else if ($("a").attr("id") === "asmarterwaytolearn") {
-    
-//  }
-//  else {
-    
-//  }
-//}
   
