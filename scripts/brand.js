36eaa58eef485a6697a817bf1a8f226539d2f3a4
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-26 17:11:03 -0500

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -10,7 +10,7 @@ function loadXMLDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("dynamiccontent").innerHTML =
+      document.getElementById("dynamiclinks").innerHTML =
       this.responseText;
     }
   };
