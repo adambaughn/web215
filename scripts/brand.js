60dee8595cf73cf9f4dcf55a9956f26e162c634c
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-02-28 22:18:45 -0500

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -62,7 +62,7 @@ function loadAsmarterWayToLearn() {
 
 function showFunctionOne() {
 var x = document.getElementById("click_one");
-var buttonText = document.getElementById("click_one");
+var buttonText = document.getElementById("click_item_one");
 if (x.style.display === "none") {
     x.style.display = "block";
     buttonText.innerHTML = "Click to hide A Smarter Way to Learn JS Chapter 1";
