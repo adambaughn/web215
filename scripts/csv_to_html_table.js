637020554c1a092d38f8430bd8f01fb282f9e7c2
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-04-26 12:12:47 -0400

    Update csv_to_html_table.js

--- scripts/csv_to_html_table.js
+++ scripts/csv_to_html_table.js
@@ -77,7 +77,27 @@ function tableFilter() {
       }
     }       
   }
-}	
+}
+
+function tableFilterLinks() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementsByClassName("tableLinksFilter");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table-container-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
 
 /*
 function pullTable(userClick) {
