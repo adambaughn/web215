8bc413f36da649cc7f4834e2b14de431e76b1bcc
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-04 23:26:17 -0400

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -372,7 +372,7 @@ function WriteCookie(value) {
                var now = new Date();
                now.setMonth( now.getMonth() + 1 );
                cookievalue = escape(value) + ";";
-               document.cookie = "name=" + cookievalue;
+               document.cookie = "username=" + cookievalue;
                document.cookie = "expires=" + now.toUTCString() + ";"
                document.getElementById("display_cookies_one").innerHTML = "Setting Cookies : " + "name=" + cookievalue ;
             }
@@ -404,7 +404,7 @@ function DeleteCookie() {
                /*
                cookievalue = escape(document.getElementById("username").value) + ";"
                */
-                document.cookie = "username=" + cookievalue;
+                document.cookie = "name=" + cookievalue;
                 document.cookie = "expires=" + now.toUTCString() + ";"            
                  document.getElementById("display_cookies_two").innerHTML = "Last Cookie: " + "name=" + cookievalue ;
             }
