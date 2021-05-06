f87efb417eb8166c6033d9a9bc6efac9488481b7
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-06 00:08:33 -0400

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -525,7 +525,12 @@ function deleteSession( ) {
 }
 
 function deleteAllCookies() {
-
+     var res = document.cookie;
+     var multiple = res.split(";");
+     for(var i = 0; i < multiple.length; i++) {
+          var key = multiple[i].split("=");
+          document.cookie = key[0]+" =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+     }
 }
 
 
