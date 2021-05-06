5f7b8452e167cbcc4ccc23932af31a7951aca1dd
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-05 23:43:18 -0400

    Update brand.js

--- scripts/brand.js
+++ scripts/brand.js
@@ -523,3 +523,14 @@ function deleteSession( ) {
     document.getElementById("display_cookies_two").innerHTML = "Deleted Cookie: session";
     alert("Deleted Cookie: session");
 }
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
